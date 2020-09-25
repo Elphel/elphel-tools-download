78147e4985c91418455249feed78a2bc4f5c1636
Oleg Dzhimiev <oleg@elphel.com>
2020-09-25 11:19:44 -0600

    minor

--- lwir/LWIR_tiff_to_jpeg.sh
+++ lwir/LWIR_tiff_to_jpeg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
 #convert 1562389844_064093_0.tiff -crop 160x120+0+0 -auto-level test.jpeg
-
+# cuts off telemetry lines in the bottom (160x122 original images)
 mogrify -crop 160x120+0+0 -auto-level -format jpeg -path ./lwir_jpeg lwir/*.tiff
